f518641c452c178fb5fff57c1bd3ab6c1f4f7b15
Acuity <Acuity1024@gmail.com>
2019-07-18 16:45:45 -0700

    Updated yes/no prompt for raw data display

--- bikeshare.py
+++ bikeshare.py
@@ -13,13 +13,13 @@ CITY_DATA = { 'Chicago': 'chicago.csv',
 MONTH_NAME = { 'January': 'jan', 'February': 'feb', 'March': 'mar',
                'April': 'apr', 'May': 'may', 'June': 'jun'}
 # Maybe do no data months after
-               #'July': 'jul', 'August': 'aug', 'September': 'sep', 
+               #'July': 'jul', 'August': 'aug', 'September': 'sep',
                #'October': 'oct', 'November': 'nov', 'December': 'dec'}
 
 MONTH_SHORT = {'jan': 'January', 'feb': 'February', 'mar': 'March',
                'apr': 'April', 'may': 'May', 'jun': 'June'}
 # For no data months
-               #'jul': 'July', 'aug': 'August', 'sep': 'September', 
+               #'jul': 'July', 'aug': 'August', 'sep': 'September',
                #'oct': 'October', 'nov': 'November', 'dec': 'December'}
 
 DAY_NAME = { 'Monday': 'mon', 'Tuesday': 'tue', 'Wednesday': 'wed',
@@ -52,12 +52,12 @@ def get_filters():
             city = city.title()
             print(city)
             break
-        
+
         elif(city.lower().title() in CITY_DATA):
             city = city.lower().title()
             print(city)
             break
-        
+
         # Shorthand for me for testing
         elif (city.lower() == 'nyc'):
             city = 'New York City'
@@ -77,10 +77,10 @@ def get_filters():
         else:
             print("{} is not a city in our dataset, please enter a valid city:".format(city).capitalize())
             print("(Example: New York City, new york city, NYC, and nyc are all acceptable formats for New York City.)")
-            
+
             # I don't think there'll be an exception from input, it happens later in the code, so we'll have to hardcode it to only accept the right input
-            
-            
+
+
     # TO DO: get user input for month (all, january, february, ... , june)
     while True:
 
@@ -88,80 +88,80 @@ def get_filters():
         if month in MONTH_SHORT:
             print(MONTH_SHORT[month])
             break
-        
+
         # handle capitalization differences
         elif month.lower() in MONTH_SHORT:
             month = month.lower()
             print(MONTH_SHORT[month])
             break
-        
+
         # can type out whole name of month
         elif month in MONTH_NAME:
             month = MONTH_NAME[month]
             print(MONTH_SHORT[month])
             break
-        
+
         # any wild capitalizations of the full month name
         elif month.lower().title() in MONTH_NAME:
             print(month.lower().title())
             month = MONTH_NAME[month.lower().title()]
             break
-        
+
         # handle capital All
         elif month == 'All':
             month = month.lower()
             print('All months')
             break
-            
+
         elif month == 'all':
             print('All months')
             break
-            
+
         else:
             print("{} is not a valid month in our dataset (we only have data from January to June), please input a valid month.".format(month).capitalize())
             print("(Example: Jan, January, jan, january are all valid formats for January.)")
-        
+
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
     while True:
         day = input("Please enter a day of week (all, mon, tue, wed, thu, fri, sat, sun):")
         if day in DAY_SHORT:
             print(DAY_SHORT[day])
             break
-        
-        
+
+
         if day.lower() in DAY_SHORT:
             print(DAY_SHORT[day])
             break
-        
+
         # set day to the shorthand if full name is typed
         if day in DAY_NAME:
             print(day)
             day = DAY_NAME[day]
             break
-        
+
         # Wild capitalization of day acceptable
         elif day.lower().title() in DAY_NAME:
             print(day.lower().title())
             day = DAY_NAME[day.lower().title()]
             break
-        
-        
+
+
         # handle capital All
         elif day == 'All':
             day = day.lower()
             print('All days')
             break
-            
+
         elif day == 'all':
             print('All days')
-            break        
-        
+            break
+
         else:
             print("{} is not a valid day in our dataset.  Please input a valid day.".format(day).capitalize())
             print("(Example: Monday, mon, Mon, monday are all valid formats for Monday.)")
-    
+
     print('-'*40)
-    
+
     return city, month, day
 
 
@@ -177,8 +177,8 @@ def load_data(city, month, day):
         df - Pandas DataFrame containing city data filtered by month and day
     """
     # load data based on the city input
-    df = pd.read_csv(CITY_DATA[city])    
-    
+    df = pd.read_csv(CITY_DATA[city])
+
     # Filter by month and day (copied from my solution to Practice 3)
         # convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
@@ -193,7 +193,7 @@ def load_data(city, month, day):
         # use the index of the months list to get the corresponding int
         months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun']
         month = months.index(month)+1
-    
+
         # filter by month to create the new dataframe
         df = df[df['month'] == month]
 
@@ -203,7 +203,7 @@ def load_data(city, month, day):
         days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
         day = days.index(day)
         df = df[df['day_of_week'] == day]
-    
+
 
     return df
 
@@ -215,36 +215,36 @@ def time_stats(df):
     start_time = time.time()
 
     # TO DO: display the most common month
-    
+
     df['month'] = df['Start Time'].dt.month
     popular_month = df['month'].mode()[0]
-    
+
     # Format popular month:
-    
-    print('Most Frequent Starting Month:', popular_month)    
-    
-    
-    
+
+    print('Most Frequent Starting Month:', popular_month)
+
+
+
 
 
 
     # TO DO: display the most common day of week
     df['weekday'] = df['Start Time'].dt.weekday
-    
+
     popular_day = df['weekday'].mode()[0]
-    
+
     # Convert this to the formatted version soon
     print('Most Frequent Starting Day of Week:', popular_day)
-    
-    
+
+
 
     # TO DO: display the most common start hour
     df['hour'] = df['Start Time'].dt.hour
     popular_hour = df['hour'].mode()[0]
-    
+
     # Format popular hour:
-    
-    print('Most Frequent Starting Hour:', popular_hour)    
+
+    print('Most Frequent Starting Hour:', popular_hour)
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
@@ -263,9 +263,9 @@ def station_stats(df):
     # TO DO: display most commonly used end station
     popular_end = df['End Station'].mode()[0]
 
-    
+
     # How do you find the most common string?
-    
+
     # TO DO: display most frequent combination of start station and end station trip
     print('The most popular start station is: ' + popular_start)
     print('The most popular destination station is: ' + popular_end)
@@ -282,7 +282,7 @@ def trip_duration_stats(df):
 
     # TO DO: display total travel time
     # I assume this means total travel time summed over all users
-    
+
     # This is in seconds for now
     totalsec = df["Trip Duration"].sum()
     print("Total travel time: {} hours, {} minutes, {} seconds.".format(totalsec//3600, totalsec%3600//60, totalsec%3600%60//1))
@@ -290,7 +290,7 @@ def trip_duration_stats(df):
     # TO DO: display mean travel time
     meansec = df["Trip Duration"].mean()
     print("Mean travel time: {} hours, {} minutes, {} seconds.".format(int(meansec//3600), int(meansec%3600//60), int(meansec%3600%60//1)))
-    
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
@@ -312,8 +312,8 @@ def user_stats(df):
     except KeyError:
         print('No gender data in dataset for this city, skipping this output.')
 
-    
-    
+
+
     # TO DO: Display earliest, most recent, and most common year of birth
     try:
         print("The oldest user was born in {}.".format(int(df['Birth Year'].min())))
@@ -321,17 +321,17 @@ def user_stats(df):
         print("The most common birth year is {}.".format(int(df['Birth Year'].mode()[0])))
     except KeyError:
         print('No birth year data in dataset for this city, skipping this output.')
-    
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-    
+
 def display_data(df):
     index = 0
     endpoint = len(df)
     while True:
-        display = input("Display 5 lines of data?")
-        if display in ['y', 'yes', 'True', 'true']:
+        display = input("Display 5 lines of data? y/n")
+        if display in ['y', 'yes', 'Y', 'Yes']:
             # handle end of dataframe
             if index+5 >= endpoint:
                 print(df.iloc[index:index+5])
